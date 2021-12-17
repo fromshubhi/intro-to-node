a8ec123ce4b9ea9705a4b4c386852790d911c60d
Shubhangi Mainalli <70970530+shubhimainalli@users.noreply.github.com>
2021-12-17 11:38:00 +0530

    added files

---
+++ index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+const supervillains = require('supervillains');
+
+var mySuperheroName = superheroes.random();
+var mySuperVillains = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySuperVillains);
